c6a319f47fa74009d40fe8c429c1dad69a226843
saipavani34 <48451768+saipavani34@users.noreply.github.com>
2021-05-28 22:33:38 +0530

    Update statspanel.js

--- UI/src/components/home/statspanel.js
+++ UI/src/components/home/statspanel.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
-//import logo from '../images/thumbs-up.jpg';
+import logo from '../images/thumbs-up.jpg';
 import '../images/bgimage.css';
 import './home.css';
 import Nav from './Nav.js';
 import { Bar } from 'react-chartjs-2';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 //import { useHistory, withRouter,Link } from "react-router-dom";
-//import { PieChart, Pie, Tooltip } from "recharts";
+import { PieChart, Pie, Tooltip } from "recharts";
 import Leaderboard from "./Leaderboard.jsx";
-import Cards from "./Cardss.jsx";
+import Cards from "./Cards.jsx";
 import styles from "./Stats.module.css";
 import ActiveRegions from "./ActiveRegions";
-import Chartt from "./Chartt.jsx";
+import Chart from "./Chart.jsx";
 
 const state = {
   labels: ['January', 'February', 'March',
@@ -63,132 +63,17 @@ const state = {
 
 
 export default class Statspanel extends Component {
-  state = {
-    transac: [],
-    pendingTransac: [],
-    water: [],
-    garbage: [],
-    road: [],
-    electricity: [],
-    active: [],
-    data1: {}
-
-  };
-  componentDidMount() {
-    this.fetchTransac();
-    this.fetchPendingTransac();
-    this.fetchWater();
-    this.fetchGarbage();
-    this.fetchRoad();
-    this.fetchElectricity();
-    this.fetchActive();
-  };
-  fetchTransac = async () => {
-    const transacCountfromDb = await fetch("http://localhost:9000/getComplaints");
-    const response = await transacCountfromDb.json();
-    this.setState({ transac: response });
-  };
-  fetchPendingTransac = async () => {
-    const transacCountfromDb = await fetch("http://localhost:9000/closedComplaints");
-    const response = await transacCountfromDb.json();
-    this.setState({ pendingTransac: response });
-  };
-  fetchWater = async () => {
-    const transacCountfromDb = await fetch("http://localhost:9000/waterComplaints");
-    const response = await transacCountfromDb.json();
-    this.setState({ water: response });
-  };
-  fetchGarbage = async () => {
-    const transacCountfromDb = await fetch("http://localhost:9000/garbageComplaints");
-    const response = await transacCountfromDb.json();
-    this.setState({ garbage: response });
-  };
-  fetchRoad = async () => {
-    const transacCountfromDb = await fetch("http://localhost:9000/roadComplaints");
-    const response = await transacCountfromDb.json();
-    this.setState({ road: response });
-  };
-  fetchElectricity = async () => {
-    const transacCountfromDb = await fetch("http://localhost:9000/ElectricityComplaints");
-    const response = await transacCountfromDb.json();
-    this.setState({ electricity: response });
-  };
-  fetchActive = async () => {
-    const transacCountfromDb = await fetch("http://localhost:9000/activeRegions");
-    const response = await transacCountfromDb.json();
-    this.setState({ active: response });
-  };
-  render() {
+    render() {
     return (<div>
       <Nav />
-
-
-
-
-      <div >
-
-        <h1 style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center"
-        }}><b>STATS PANEL</b></h1>
-        <div class="green"><h6><b><i>Total complaints</i></b></h6>
-          <h6>Total No Complaints:{this.state.transac.length}</h6>
-          <h6>No of Solved complaints:{this.state.pendingTransac.length}</h6>
-
-        </div>
-
-        <div class="red"><h6><b><i>DepartmentWiseSolvedComplaints</i></b></h6>
-          <h6>Water issues solved:{this.state.water.length}</h6>
-          <h6>Road issues solved:{this.state.road.length}</h6>
-          <h6>Electricity issues solved:{this.state.electricity.length}</h6>
-          <h6>Garbage issues solved:{this.state.garbage.length}</h6>
-        </div>
-
-        <div class="blue"><h6><b><i>Active Regions</i></b></h6>
-          <h6>{this.state.active}</h6>
-
-
-        </div>
-
-        <div class="row"><h6 class="primary"><b><i></i></b></h6>
-
-
-        </div>
-
-
-
-      </div>
-      <div style={{
-
-        alignItems: "right",
-        width: "600px",
-        height: "300px"
-
-      }}>
-        <Bar
-          data={state}
-          options={{
-            title: {
-              display: true,
-              text: 'No of complaints per month',
-              fontSize: 20
-            },
-            legend: {
-              display: true,
-              position: 'right'
-            }
-          }}
-        />
-
-      </div>
-
+    
       <div className={styles.container}>
-
+      <h1><b>STATS PANEL</b></h1>
         <br />
         <Cards  />
-
-        <Chartt />
+       
+        <Chart />
+      
         <div className="row">
         <div className="col-lg-6">
           <Leaderboard />
