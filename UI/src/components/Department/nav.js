c157f150fa641ebcf5db9159ab4df3e010836dfe
sowmya-seetha <56246381+sowmya-seetha@users.noreply.github.com>
2021-06-08 13:24:36 +0530

    Update nav.js

--- UI/src/components/Department/nav.js
+++ UI/src/components/Department/nav.js
@@ -12,7 +12,7 @@ export default class Nav extends Component{
                         <li><a href="/viewdeptcomplaints"><i className="fas fa-address-card"></i>View Complaints</a></li>
                         {/* <li><a href="/pendingcomplaints"><i className="fas fa-blog"></i>Pending Complaints</a></li>
                         <li><a href="/closedcomplaints"><i className="fas fa-address-book"></i>Closed Complaints</a></li> */}
-                         <li><a href="/deptmarker"><i className="fas fa-address-card"></i>Map View</a></li>
+                        
                         <li><a href="/viewdeptusers"><i className="fas fa-address-card"></i>View Users</a></li>
                         {/* <li><a href="/deptleaderboard"><i className="fas fa-address-card"></i>Leaderboard</a></li>
                         <li><a href="/recentlycreated"><i className="fas fa-address-card"></i>Recently Created</a></li>
